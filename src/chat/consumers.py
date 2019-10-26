49b879418c471d75a57e74ee16d32689c99a141f
maximfreerider <ivanovma41@gmail.com>
2019-10-26 13:15:55 +0300

    Added some async funct to proj

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,16 +1,47 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import (WebsocketConsumer, AsyncJsonWebsocketConsumer)
+from asgiref.sync import async_to_sync
 import json
 
-class ChatComsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+
+class AsyncChatConsumer(AsyncJsonWebsocketConsumer):
+
+    async def connect(self):
+        self.murr_chat_name = self.scope['url_route']['kwargs']['murr_chat']
+        await async_to_sync(self.channel_layer.group_add)(self.murr_chat_name, self.channel_name)
+        await self.accept()
 
     def disconnect(self, code):
         pass
+        pass
+
+    async def receive_json(self, content, **kwargs):
+        # content["!"] = 1
+        await self.send_json(content=content)
+
+    @classmethod
+    async def encode_json(cls, content):
+        return await super().encode_json(content)
+
+    @classmethod
+    async def decode_json(cls, text_data):
+        return await super().decode_json(text_data)
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        print("It`s connected!")
 
     def receive(self, text_data=None, bytes_data=None):
+        if text_data:
+            print("u have new message")
+        else:
+            print("nothing")
         json_data = json.loads(text_data)
-        message = json_data['message']
+        # message = json_data['data']
         self.send(text_data=json.dumps({
-            'message': "hello from backend!!!"
+            'data': "hello from backend!!!"
         }))
+
+    def disconnect(self, code):
+        pass
--- src/chat/routing.py
+++ src/chat/routing.py
@@ -1,6 +1,7 @@
-from .consumers import ChatComsumer
-from django.urls import path, re_path
+from .consumers import ChatConsumer, AsyncChatConsumer
+from django.urls import re_path
 
 websocket_urls = [
-    re_path(r'^ws/chat/$', ChatComsumer),
+    re_path(r'^ws/chat/$', ChatConsumer),
+    re_path(r'^ws/async_chat/<murr_chat_name>/$', AsyncChatConsumer),
 ]
